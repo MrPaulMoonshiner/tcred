d85f3ffed5e6adea09918f8236d2d162ebca031d
Pavlo.S.G <pavlo.sichkar@ua.nestle.com>
2024-09-10 17:09:14 +0300

    add counter

--- counter/counter.sh
+++ counter/counter.sh
@@ -1,31 +1,25 @@
 #!/bin/bash
 
-# COUNTER_FILE="$HOME/tcred/counter/storage.txt"
-
-# count=$(cat $COUNTER_FILE)
-# count=$((count + 1))
-# echo $count > $COUNTER_FILE
-
 FILE="$HOME/tcred/counter/storage"
 
 DATE=$(date +"%Y-%m-%d")
 
-# Ініціалізація масиву
+
 declare -A run_counts
 
-# Функція для зчитування даних з файлу
+
 read_data() {
     if [ -f "$FILE" ]; then
         while IFS="=" read -r key value; do
             run_counts["$key"]=$value
         done < "$FILE"
     else
-        # Якщо файл не існує, створюємо його
+
         touch "$FILE"
     fi
 }
 
-# Функція для запису даних у файл
+
 write_data() {
     > "$FILE"
     for key in "${!run_counts[@]}"; do
@@ -33,15 +27,13 @@ write_data() {
     done
 }
 
-# Зчитуємо дані з файлу
+
 read_data
 
-# Збільшуємо кількість запусків для поточної дати
 if [ -n "${run_counts[$DATE]}" ]; then
     run_counts[$DATE]=$((run_counts[$DATE] + 1))
 else
     run_counts[$DATE]=1
 fi
 
-# Записуємо оновлені дані у файл
 write_data
--- counter/counter.zsh
+++ counter/counter.zsh
@@ -2,36 +2,28 @@
 
 FILE="$HOME/tcred/counter/storage"
 
-# Отримання поточної дати
 DATE=$(date +"%Y-%m-%d")
 
-# Ініціалізація змінної для зберігання даних
 run_counts=""
 
-# Функція для зчитування даних з файлу
 read_data() {
     if [ -f "$FILE" ]; then
         run_counts=$(cat "$FILE")
     else
-        # Якщо файл не існує, створюємо його
         touch "$FILE"
     fi
 }
 
-# Функція для запису даних у файл
 write_data() {
     echo "$run_counts" > "$FILE"
 }
 
-# Зчитуємо дані з файлу
 read_data
 
-# Збільшуємо кількість запусків для поточної дати
 if echo "$run_counts" | grep -q "$DATE"; then
     run_counts=$(echo "$run_counts" | sed -E "s/($DATE=)([0-9]+)/echo \1$((\2 + 1))/e")
 else
     run_counts="$run_counts\n$DATE=1"
 fi
 
-# Записуємо оновлені дані у файл
 write_data
