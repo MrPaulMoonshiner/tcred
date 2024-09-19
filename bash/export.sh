18506b370f48e700af93336215f983c6c520f1e3
Pavlo.S.G <pavlo.sichkar@ua.nestle.com>
2024-09-19 16:16:17 +0300

    db export

--- bash/export.sh
+++ bash/export.sh
@@ -4,9 +4,9 @@ TCRED_STORAGE_DIR="$HOME/tcred/storage"
 source $TCRED_STORAGE_DIR/key_value_storage
 
 terminus_export() {
-    
+
     if [[ -z ${url_aliases[$2]} ]]; then
-        echo "Error: Unknown URL alias '$alias'"
+        echo "Error: Unknown URL alias '$2'"
         exit 1
     fi
     CURRENT_DATE=$(date +"%Y_%m_%d")
