ddd893dbabfcdce0008d32f75d6ac9d6d2f6b305
Pavlo.S.G <pavlo.sichkar@ua.nestle.com>
2024-09-19 15:42:57 +0300

    db export

--- zsh/export.zsh
+++ zsh/export.zsh
@@ -6,7 +6,7 @@ source $TCRED_STORAGE_DIR/key_value_storage
 terminus_export() {
     local util='backup:get'
     local url=${url_aliases[$2]}
-    local env=$3
+    local env=${3:-live}
     local flag_path="--to=$(eval echo ${4:-./})"
     local flag_element="--element=${5:-db}"
     echo "Command to run: terminus $util $url.$env $flag_element $flag_path"
