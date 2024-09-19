43ba84088eb800ca61854716be0adb324dc961c3
Pavlo.S.G <pavlo.sichkar@ua.nestle.com>
2024-09-19 16:09:30 +0300

    db export

---
+++ bash/export.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TCRED_STORAGE_DIR="$HOME/tcred/storage"
+source $TCRED_STORAGE_DIR/key_value_storage
+
+terminus_export() {
+    CURRENT_DATE=$(date +"%Y_%m_%d")
+    local util='backup:get'
+    local url=${url_aliases[$2]}
+    local env=${3:-live}
+    local flag_path="--to=$(eval echo ${4:-./$CURRENT_DATE.tar.gz})"
+    local flag_element="--element=${5:-db}"
+    echo "Command to run: terminus $util $url.$env $flag_element $flag_path"
+    terminus "$util" "$url.$env" "$flag_element" "$flag_path"
+
+}
+
+terminus_export "$1" "$2" "$3" "$4" "$5"
