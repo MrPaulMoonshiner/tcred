886f0307d276c475b0136f7476e86715a51e3c74
Pavlo.S.G <pavlo.sichkar@ua.nestle.com>
2024-09-20 09:34:34 +0300

    help update

--- bash/export.sh
+++ bash/export.sh
@@ -13,7 +13,7 @@ terminus_export() {
     local util='backup:get'
     local url=${url_aliases[$2]}
     local env=${3:-live}
-    local flag_path="--to=$(eval echo ${4:-./$CURRENT_DATE.sql.gz})"
+    local flag_path="--to=$(eval echo ${4:-./$env.$CURRENT_DATE.sql.gz})"
     local flag_element="--element=${5:-db}"
     echo "Command to run: terminus $util $url.$env $flag_element $flag_path"
     terminus "$util" "$url.$env" "$flag_element" "$flag_path"
